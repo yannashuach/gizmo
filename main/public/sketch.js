3312c771ba8176a55d5f38bc016ca0d4073453c9
yannashuach <yannashuach@gmail.com>
2022-12-14 10:24:22 +0000

    miscellaneous commit

--- main/public/sketch.js
+++ main/public/sketch.js
@@ -1,3 +1,8 @@
+//Serial Stuff
+var serial;   // variable to hold an instance of the serialport library
+var portName = '/dev/tty.usbmodem14401';    // fill in your serial port name here
+
+
 var socket;
 let video;
 let facemesh;
@@ -8,6 +13,17 @@ let normalized = new Array(468*3);
 var emotions;
 
 function setup() {
+  //Set up serial communication port
+  serial = new p5.SerialPort();       // make a new instance of the serialport library
+  serial.on('list', printList);  // set a callback function for the serialport list event
+  serial.on('connected', serverConnected); // callback for connecting to the server
+  serial.on('open', portOpen);        // callback for the port opening
+  serial.on('data', serialEvent);     // callback for when new data arrives
+  serial.on('error', serialError);    // callback for errors
+  serial.on('close', portClose);      // callback for the port closing
+  serial.list();                      // list the serial ports
+  serial.open(portName);              // open a serial port
+
   socket = io.connect(window.location.origin);
   const canvas = createCanvas(640, 480);
   canvas.parent("videoContainer");
@@ -32,31 +48,36 @@ function setup() {
     function(data) {
       emotions = data.args[0].value;
 
-      for(var n = 0; n < data.args.length; n++) {
-        println(n + ": " + data.args[n].value);
-      }
+      // for(var n = 0; n < data.args.length; n++) {
+      //   println(n + ": " + data.args[n].value);
+      // }
     }
   );
+  hellotest();
 }
 
 
 function draw() {
   image(video, 0, 0, width, height);
   if(emotions==1) {
-    text('Neutral', 10, 30);
+    text('Calm', 10, 30);
   } else if (emotions==2){
     text('Happy', 10, 30); 
   } else if (emotions==3){
     text('Suprised', 10, 30); 
+  } else if (emotions==4) {
+    text('Angry', 10, 30); 
   }
 
 
  // We can call both functions to draw all keypoints and the skeletons
  drawBoxes();
  drawKeypoints();
+ setInterval(hellotest(), 1000);
 }
 
-function mouseMoved() {
+async function hellotest() {
+  await sleep(1000)
   for (let j = 0; j < predictions[0].scaledMesh.length*3; j += 3) {
     const i = j/3;
     normalized[j]={type: "float", value: predictions[0].scaledMesh[i][0]-predictions[0].boundingBox.topLeft[0][0]};
@@ -64,9 +85,17 @@ function mouseMoved() {
     normalized[j+2]={type: "float", value: predictions[0].scaledMesh[i][2]};
 
   }
-  
   socket.emit('inputData', normalized);
+  //Serial Setup
+  serial.write(emotions);
   // setInterval(function () {socket.emit('inputData', { x: predictions[0].boundingBox.topLeft[0][0], y: predictions[0].boundingBox.topLeft[0][1]})}, 1000);
+}
+
+// a custom 'sleep' or wait' function, that returns a Promise that resolves only after a timeout
+function sleep(millisecondsDuration) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecondsDuration);
+  })
 }
 
 
@@ -102,3 +131,32 @@ function drawKeypoints() {
 function modelReady() {
   select("#status").html("model Loaded");
 }
+
+// Following functions print the serial communication status to the console for debugging purposes
+function printList(portList) {
+  // portList is an array of serial port names
+  for (var i = 0; i < portList.length; i++) {
+  // Display the list the console:
+  print(i + " " + portList[i]);
+  }
+ }
+ 
+ function serverConnected() {
+   print('connected to server.');
+ }
+ 
+ function portOpen() {
+   print('the serial port opened.')
+ }
+ 
+ function serialEvent() {
+   inData = Number(serial.read());
+ }
+ 
+ function serialError(err) {
+   print('Something went wrong with the serial port. ' + err);
+ }
+ 
+ function portClose() {
+   print('The serial port closed.');
+ }
